b1b7313145c194421f1b34f75ceafd811f364e04
AaronScott96 <s01125467@acad.tri-c.edu>
2020-03-23 21:23:19 -0400

    hello world

---
+++ Assignment 1/Assignment 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#pragma warning disable CS0168 // Variable is declared but never used
+            string Fullname;
+#pragma warning restore CS0168 // Variable is declared but never used
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
+            Fullname = "firstname" + "middlename" + "lastname";
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
