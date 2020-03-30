40a2fdc68978c3e480dca3c80bb14537019cf9cc
AaronScott96 <s01125467@acad.tri-c.edu>
2020-03-30 19:51:57 -0400

    new program

---
+++ LAB 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LAB_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int  sum;
+            Console.Write("Enter first integer: ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second integer: ");
+            number2 = int.Parse(Console.ReadLine());
+            sum = number1 + number2;
+            Console.WriteLine($"Sum is {sum}");
+
+            Console.WriteLine("Hello World!\nFrom Aaron Scott");
+            Console.Write("Hello world!         From Aaron Scott");
+            
+            
+
+
+        }
+    }
+}
