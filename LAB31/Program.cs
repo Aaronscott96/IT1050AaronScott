683524ceef52c51d4385ed8c38435bb6b840d657
AaronScott96 <s01125467@acad.tri-c.edu>
2020-04-07 12:37:55 -0400

    edited lab3

--- LAB31/Program.cs
+++ LAB31/Program.cs
@@ -8,35 +8,78 @@ namespace LAB31
 
         static void Main(string[] args)
         {
-           
 
-
-            int age = 10;
-            if (age > 60)
-            { 
-                Console.WriteLine("old more than 60"); 
-            }
-            else
-
-            if (age < 60)
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
             {
-                Console.WriteLine(" young less than 60");
-            }
-            else
-            {
-                Console.WriteLine("old equals 60");
-            }
-            {
-              
-             
-            
+                double celsius = 0;
 
-                 
-                
-                     
+                if (fahrenheit < 45)
+                {
+                    Console.WriteLine("it is cold");
+                }
+                else if (fahrenheit >= 90)
+                {
+                    Console.WriteLine("it is hot");
+
+                }
+
+
+                celsius = (fahrenheit - 32d) * 5d / 9d;
+
+
+
             }
-    }   }
+
+
+            int grade = 59;
+            if (grade >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else if (grade >= 0 - 59)
+            {
+                Console.WriteLine("Failed");
+            }
+
+            //             int speed = 42;
+
+           // int speedlimit = 35;
+            //if (speedlimit < speed)
+            //{
+                Console.WriteLine("slow now");
+            // }
+            //    string A = "Cat";
+           // string B = "cat";
+          //  int answers = 0;
+           // answers = string.Compare(A, B);
+           // Console.WriteLine(answers.ToString());
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
 }
-;
+
 
           
