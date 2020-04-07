77e341c30ac76964f91a85c971d9f984e4a75a80
AaronScott96 <s01125467@acad.tri-c.edu>
2020-04-06 23:17:34 -0400

    new stuff

---
+++ LAB31/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LAB31
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+           
+
+
+            int age = 10;
+            if (age > 60)
+            { 
+                Console.WriteLine("old more than 60"); 
+            }
+            else
+
+            if (age < 60)
+            {
+                Console.WriteLine(" young less than 60");
+            }
+            else
+            {
+                Console.WriteLine("old equals 60");
+            }
+            {
+              
+             
+            
+
+                 
+                
+                     
+            }
+    }   }
+}
+;
+
+          
---
+++ Lab3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void
+            Console.Write("Enter first integer: 60 ");
+        
+        if 
+          
+       
+    }
+}
