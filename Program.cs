797432f84aa04abb7a4630416c196cb72161a1f7
Aaronscott96 <62576207+Aaronscott96@users.noreply.github.com>
2020-04-13 15:20:21 -0400

    new stuff lab 4

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace just_for_fun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // int i;
+
+
+
+            //   for (i = 1; i < 101;  i = i + 1) 
+
+            {//
+
+                //       Console.WriteLine (i);
+
+                //     }
+                //   if ((i % 2) == 0)
+                //    {//
+                //  Console.WriteLine("its even");
+                //  }
+                //    else if  ((i % 3) == 0)
+                //   {
+                //       Console.WriteLine("its odd");
+            }
+            {
+                //  Console.Write("Please enter a temperature: ");
+              //  int temperature = Convert.ToInt32(Console.ReadLine());
+               // if (temperature >= 90)
+              //      Console.WriteLine("Fish");
+              //  else if (temperature >= 80)
+              //      Console.WriteLine("Lion");
+             //   else if (temperature >= 70)
+             //       Console.WriteLine("Turtle");
+              //  else if (temperature >= 60)
+              //      Console.WriteLine("Deer");
+              //  else if (temperature >= 50)
+                //    Console.WriteLine("Reindeer");
+              //  else if (temperature >= 40)
+                 //   Console.WriteLine("Moose");
+              //  else if (temperature >= 20)
+              //      Console.WriteLine("Penguin");
+             //   else if (temperature >= 10)
+               //     Console.WriteLine("Polar Bear");
+              //  else if (temperature >= 100)
+                //    Console.WriteLine("Bug"); 
+            }
+            {
+                //  Console.Write("Please enter the exhibit number:  ");
+                //  int animal = Convert.ToInt32(Console.ReadLine());
+                //  switch (animal)
+
+
+                {
+                    //  case 9:
+                    //     Console.WriteLine("Bug");
+                    //       break;
+                    //     case 8:
+                    //    Console.WriteLine("Fish");
+                    //      break;
+                    //    case 7:
+                    //  Console.WriteLine("Lion");
+                    //    break;
+                    //  case 6:
+                    //   Console.WriteLine("Turtle");
+                    //    break;
+                    //  case 5:
+                    //      Console.WriteLine("Deer");
+                    //    break;
+                    //  case 4:
+                    //     Console.WriteLine("Reindeer");
+                    //      break;
+                    //    case 3:
+                    //      Console.WriteLine("Moose");
+                    //   break;
+                    //   case 2:
+                    //     Console.WriteLine("Penguin");
+                    //       break;
+                    ////       case 1:
+                    //     Console.WriteLine("Polar Bear");
+                    //           break; *//
+
+                }
+                {
+
+                    int i = 10;
+                    do
+                    {
+                        Console.WriteLine(i++);
+                    }
+                    while (i < 21);
+
+                }
+            }    
+                   /* for (int i = 0; i < 101; i++)
+
+                    {
+                        Console.WriteLine(i);
+                        Console.WriteLine("********");
+                    }
+                
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
+
+    
+            
+       
+         
